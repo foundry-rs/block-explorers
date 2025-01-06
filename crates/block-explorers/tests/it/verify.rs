5b54adebc1d116f28487482d70fd0f9f01e714ab
iain nash <me@iain.in>
2025-01-06 12:35:41 -0500

    add standard-input-json test and fix integration tests

--- crates/block-explorers/tests/it/verify.rs
+++ crates/block-explorers/tests/it/verify.rs
@@ -2,17 +2,15 @@
 
 use crate::run_with_client;
 use alloy_chains::Chain;
-use foundry_block_explorers::verify::VerifyContract;
+use foundry_block_explorers::verify::{VerifyContract, CodeFormat};
 use foundry_compilers::{compilers::solc::{SolcCompiler, SolcLanguage}, Project, ProjectPathsConfig};
 use serial_test::serial;
 use std::path::Path;
 
-// #[ignore]
-
 #[tokio::test]
 #[serial]
-async fn can_flatten_and_verify_contract() {
-    let root = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/../../../tests/testdata/uniswap"));
+async fn can_flatten_and_verify_contract_single_file() {
+    let root = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/test-data/rewards"));
     let paths = ProjectPathsConfig::builder()
         .sources(root)
         .build()
@@ -22,20 +20,58 @@ async fn can_flatten_and_verify_contract() {
         .build(Default::default())
         .expect("failed to build the project");
 
-    let address = "0x9e744c9115b74834c0f33f4097f40c02a9ac5c33".parse().unwrap();
-    let compiler_version = "v0.5.17+commit.d19bba13";
-    let constructor_args = "0x000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000005f5e1000000000000000000000000000000000000000000000000000000000000000007596179537761700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000035941590000000000000000000000000000000000000000000000000000000000";
+    let address = "0x7777777F279eba3d3Ad8F4E708545291A6fDBA8B".parse().unwrap();
+    let compiler_version = "v0.8.17+commit.8df45f5f";
     let contract = project
         .paths
         .clone()
-        .with_language::<SolcLanguage>().flatten(&root.join("UniswapExchange.sol"))
+        .with_language::<SolcLanguage>().flatten(&root.join("ProtocolRewards.sol"))
         .expect("failed to flatten contract");
-    let contract_name = "UniswapExchange".to_owned();
+    let contract_name = "ProtocolRewards.sol:ProtocolRewards".to_owned();
     let contract =
         VerifyContract::new(address, contract_name, contract, compiler_version.to_string())
-            .constructor_arguments(Some(constructor_args))
+            .code_format(CodeFormat::SingleFile) 
             .optimization(true)
-            .runs(200);
+            .runs(500000);
+
+    run_with_client(Chain::mainnet(), |client| async move {
+        let resp = client
+            .submit_contract_verification(&contract)
+            .await
+            .expect("failed to send the request");
+        // `Error!` result means that request was malformatted
+        assert_ne!(resp.result, "Error!", "{resp:?}");
+        assert_ne!(resp.message, "NOTOK", "{resp:?}");
+    })
+    .await
+}
+
+#[tokio::test]
+#[serial]
+async fn can_verify_contract_json_multi_file() {
+    let root = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/test-data/rewards"));
+
+    let paths = ProjectPathsConfig::builder()
+        .sources(root)
+        .build()
+        .expect("failed to resolve project paths");
+    let project = Project::builder()
+        .paths(paths)
+        .build(Default::default())
+        .expect("failed to build the project");
+
+    let address = "0x7777777F279eba3d3Ad8F4E708545291A6fDBA8B".parse().unwrap();
+    let compiler_version = "v0.8.17+commit.8df45f5f";
+    let contract = project
+        .clone()
+        .standard_json_input(&root.join("ProtocolRewards.sol"));
+    let contract_name = "ProtocolRewards.sol:ProtocolRewards".to_owned();
+    let contract_json_string = serde_json::to_string(&contract.expect("Contract did not compile to standard json"));
+    let contract =
+        VerifyContract::new(address, contract_name, contract_json_string.expect("Did not serialize json from standard json output"), compiler_version.to_string())
+            .code_format(CodeFormat::SingleFile) 
+            .optimization(true)
+            .runs(500000);
 
     run_with_client(Chain::mainnet(), |client| async move {
         let resp = client
